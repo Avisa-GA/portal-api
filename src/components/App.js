82f76370247e51a2d5b22a4042d1a58cb4ed45d9
Avisa-SEI <aap571420@gmail.com>
2021-09-07 16:57:39 -0700

    wip: create a new component for side news

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,10 @@ class App extends React.Component {
         type: "everything",
         query: "q=apple&from=2021-09-06&to=2021-09-06&sortBy=popularity",
       },
+      news3: {
+        type: "everything",
+        query: "domains=comicbookmovie.com&language=en",
+      },
     };
   }
   render() {
@@ -23,7 +27,7 @@ class App extends React.Component {
         <div className="navbar-fixed">
           <nav>
             <div className="nav-wrapper indigo lighten-4">
-              <a href="#!" className="bran-logo center">
+              <a href="/" className="bran-logo center">
                 My Feed
               </a>
             </div>
@@ -35,7 +39,7 @@ class App extends React.Component {
             <News news={this.state.news2} />
           </div>
           <div className="col s4">
-            <SideNews />
+            <SideNews news={this.state.news3} />
           </div>
         </div>
       </div>
