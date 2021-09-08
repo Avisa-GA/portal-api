bf9c3003d3513cd39c992016e09198f1eee14fb4
Avisa-SEI <aap571420@gmail.com>
2021-09-07 17:15:21 -0700

    wip: fetch with axios

--- src/components/SideNews/SideNews.js
+++ src/components/SideNews/SideNews.js
@@ -1,22 +1,42 @@
 import React from "react";
+import axios from "axios";
 import SingleSideNews from "./SingleSideNews";
+import Error from "./Error";
 
 class SideNews extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      news: [],
+      sidenews: [],
+      error: false,
     };
   }
 
   componentDidMount() {
-    // const url = `https://newsapi.org/v2/${this.props.news.type}?${this.props.news.query}&apiKey=c423045c1c954de98b2753233d74b768`;
+    const url = `https://newsapi.org/v2/${this.props.news.type}?${this.props.news.query}&apiKey=c423045c1c954de98b2753233d74b768`;
+
+    axios
+      .get(url)
+      .then((response) => {
+        this.setState({
+          sidenews: response.data.articles,
+        });
+      })
+      .catch((err) => {
+        this.setState({
+          error: true,
+        });
+      });
   }
 
   renderItems() {
-    return this.state.news.map((item) => (
-      <SingleSideNews key={item.url} item={item} />
-    ));
+    if (!this.state.error) {
+      return this.state.sidenews.map((item) => (
+        <SingleSideNews key={item.url} item={item} />
+      ));
+    } else {
+      return <Error />;
+    }
   }
 
   render() {
--- src/components/SideNews/SingleSideNews.js
+++ src/components/SideNews/SingleSideNews.js
@@ -4,7 +4,7 @@ export default function SingleSideNews({ item }) {
   return (
     <div>
       <div className="divider"></div>
-      <a href={item.url} target="_blank">
+      <a href={item.url} target="_blank" rel="noreferrer">
         <div className="section">
           <h5>{item.source.name}</h5>
           <p>{item.title}</p>
